f1606a004a6e31db81357537605bccaedcf92792
Raf Swiderski <rafswid@icloud.com>
2021-04-08 16:42:24 +0100

    delete Leeroys original code that was commented out

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -62,69 +62,11 @@ class GildedRose
       else
         decrease_quality(item, 1)
       end
-      
 
-
-
-      # if item.name == "Aged Brie"
-      #   increase_quality(item, 1)
-      # end
-
-
-    end #<-- extra ends
+    end 
   end
 end
-=begin
 
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
-
-  end
-
-end
-=end
 class Item
   attr_accessor :name, :sell_in, :quality
 
